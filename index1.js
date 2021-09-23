4444226b2e560b5c9c4f0236cf2e90bc27172503
Abhijeet Kumar <83833440+abhijeetnishal@users.noreply.github.com>
2021-09-23 17:46:46 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,73 @@
+var a=1;
+function showorhide1(){
+    if(a==1){
+        document.getElementById("greyboxout-1").style.display="flex";
+        document.getElementById('plus-btn1').style.transform='rotate(45deg)';
+        a=0;
+    }
+    else{
+        document.getElementById("greyboxout-1").style.display="none";
+        document.getElementById('plus-btn1').style.transform='rotate(90deg)';
+        a=1;
+    }
+}
+function showorhide2(){
+    if(a==1){
+        document.getElementById('greyboxout-2').style.display="flex";
+        document.getElementById('plus-btn2').style.transform='rotate(45deg)';
+        a=0;
+    }
+    else{
+        document.getElementById("greyboxout-2").style.display="none";
+        document.getElementById('plus-btn2').style.transform='rotate(90deg)';
+        a=1;
+    }
+}
+function showorhide3(){
+    if(a==1){
+        document.getElementById('greyboxout-3').style.display="flex";
+        document.getElementById('plus-btn3').style.transform='rotate(45deg)';
+        a=0;
+    }
+    else{
+        document.getElementById("greyboxout-3").style.display="none";
+        document.getElementById('plus-btn3').style.transform='rotate(90deg)';
+        a=1;
+    }
+}
+function showorhide4(){
+    if(a==1){
+        document.getElementById('greyboxout-4').style.display="flex";
+        document.getElementById('plus-btn4').style.transform='rotate(45deg)';
+        a=0;
+    }
+    else{
+        document.getElementById("greyboxout-4").style.display="none";
+        document.getElementById('plus-btn4').style.transform='rotate(90deg)';
+        a=1;
+    }
+}
+function showorhide5(){
+    if(a==1){
+        document.getElementById('greyboxout-5').style.display="flex";
+        document.getElementById('plus-btn5').style.transform='rotate(45deg)';
+        a=0;
+    }
+    else{
+        document.getElementById("greyboxout-5").style.display="none";
+        document.getElementById('plus-btn5').style.transform='rotate(90deg)';
+        a=1;
+    }
+}
+function showorhide6(){
+    if(a==1){
+        document.getElementById('greyboxout-6').style.display="flex";
+        document.getElementById('plus-btn6').style.transform='rotate(45deg)';
+        a=0;
+    }
+    else{
+        document.getElementById("greyboxout-6").style.display="none";
+        document.getElementById('plus-btn6').style.transform='rotate(90deg)';
+        a=1;
+    }
+}
